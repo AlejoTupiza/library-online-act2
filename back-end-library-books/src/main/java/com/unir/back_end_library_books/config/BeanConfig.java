fb34e5b983a02a3c18b03c6d8b823cd8198f9a43
Gonzalo Moncada <gmitiweb@gmail.com>
2024-06-07 17:28:48 -0500

    bean config

---
+++ back-end-library-books/src/main/java/com/unir/back_end_library_books/config/BeanConfig.java
@@ -0,0 +1,12 @@
+package com.unir.back_end_library_books.config;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
